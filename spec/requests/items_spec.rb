40d324e1c4537289d6743cdfbffbc519e274f618
Kushizaki <kushimegu288@gmail.com>
2025-10-20 14:52:17 +0900

    itemsのrequest_specにログイン処理を追加

--- spec/requests/items_spec.rb
+++ spec/requests/items_spec.rb
@@ -13,17 +13,11 @@ require 'rails_helper'
 # sticking to rails and rspec-rails APIs to keep things simple and stable.
 
 RSpec.describe "/items", type: :request do
-  
-  # This should return the minimal set of attributes required to create a valid
-  # Item. As you add validations to Item, be sure to
-  # adjust the attributes here as well.
-  let(:valid_attributes) {
-    skip("Add a hash of attributes valid for your model")
-  }
+  let(:user) { FactoryBot.create(:user, uid: "1234567890") }
+  let(:valid_attributes) { skip("Add a hash of attributes valid for your model") }
+  let(:invalid_attributes) { skip("Add a hash of attributes invalid for your model") }
 
-  let(:invalid_attributes) {
-    skip("Add a hash of attributes invalid for your model")
-  }
+  before { login user }
 
   describe "GET /index" do
     it "renders a successful response" do
@@ -74,7 +68,7 @@ RSpec.describe "/items", type: :request do
       it "does not create a new Item" do
         expect {
           post items_url, params: { item: invalid_attributes }
-        }.to change(Item, :count).by(0)
+        }.not_to change(Item, :count)
       end
 
       it "renders a response with 422 status (i.e. to display the 'new' template)" do
