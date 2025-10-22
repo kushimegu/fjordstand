2d5f700f3528a7a554db34cf5186eaa2ee1fc142
Kushizaki <kushimegu288@gmail.com>
2025-10-22 11:59:19 +0900

    出品フォームにバリデーション追加

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,9 +2,9 @@ class Item < ApplicationRecord
   belongs_to :user
   has_many_attached :images
 
-  validate :image_content_type
-  validate :image_size
-  before_validation :set_entry_deadline_at_end_of_day
+  validates :title, :price, :payment_method, :entry_deadline_at, presence: true
+  validate :image_content_type, :image_size, :image_length
+  before_save :set_entry_deadline_at_end_of_day
 
   enum :shipping_fee_payer, { buyer: 0, seller: 1 }
   enum :status, { draft: 0, published: 1, sold: 2, closed: 3 }
@@ -15,8 +15,9 @@ class Item < ApplicationRecord
     return unless images.attached?
 
     images.each do |image|
-      unless image.content_type.in?(%w[image/jpeg image/png image/gif])
-        errors.add(:image, "：ファイル形式が、JPEG, PNG, GIF以外になっています。ファイル形式をご確認ください。")
+      if !image.content_type.in?(%w[image/jpeg image/png])
+        image.purge
+        errors.add(:image, "のJPEGかPNGのファイルを選択してください")
       end
     end
   end
@@ -25,9 +26,20 @@ class Item < ApplicationRecord
     return unless images.attached?
 
     images.each do |image|
-      unless image.blob.byte_size > 1.megabytes
-        errors.add(:image, "：1MB以下のファイルをアップロードしてください。")
+      if image.blob.byte_size > 5.megabytes
+        image.purge
+        errors.add(:image, "は5MB以下のファイルをアップロードしてください")
       end
+    end
+  end
+
+  def image_length
+    if images.length > 5
+      images.purge
+      errors.add(:images, "は5枚以内にしてください")
+    end
+    if images.length < 1
+      errors.add(:images, "を追加してください")
     end
   end
 
