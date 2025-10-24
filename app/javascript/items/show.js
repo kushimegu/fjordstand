d5e2a3eaa4fcd63b0ddb5760ff2776cc82c52c3e
Kushizaki <kushimegu288@gmail.com>
2025-10-24 13:27:43 +0900

    商品画像のスライド表示を追加

--- app/javascript/items/show.js
+++ app/javascript/items/show.js
@@ -1,8 +1,19 @@
 document.addEventListener('turbo:load', () => {
+  const bigImage = document.getElementById('big-image');
   const thumbs = document.querySelectorAll('.thumbnail');
-  thumbs.forEach(function (item) {
+  let currentIndex = 0;
+  thumbs.forEach(function (item, index) {
     item.addEventListener('click', function () {
-      document.getElementById('big-image').src = this.dataset.image;
+      bigImage.src = this.dataset.image;
+      currentIndex = index;
     });
+  });
+  document.getElementById('next').addEventListener('click', function () {
+    currentIndex = (currentIndex + 1) % thumbs.length;
+    bigImage.src = thumbs[currentIndex].dataset.image;
+  });
+  document.getElementById('prev').addEventListener('click', function () {
+    currentIndex = (currentIndex - 1 + thumbs.length) % thumbs.length;
+    bigImage.src = thumbs[currentIndex].dataset.image;
   });
 });
