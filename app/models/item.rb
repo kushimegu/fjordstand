ef7285d617869a36768fd58b8e00fe8028905616
Kushizaki <kushimegu288@gmail.com>
2025-11-13 17:07:07 +0900

    出品後は情報の追記と締切の延長のみ行えるようにした

--- app/models/item.rb
+++ app/models/item.rb
@@ -6,9 +6,12 @@ class Item < ApplicationRecord
   validates :images, attached: { message: "を選択してください" }, on: :publish
   validates :images, limit: { max: 5 }, content_type: [ "image/png", "image/jpeg" ], size: { less_than: 5.megabytes }
   before_save :set_entry_deadline_at_end_of_day
+  validate :price_not_change_when_published, :deadline_later_than_today, :deadline_later_than_original_when_published
 
   enum :shipping_fee_payer, { buyer: 0, seller: 1 }
   enum :status, { draft: 0, published: 1, sold: 2, closed: 3 }
+
+  attr_accessor :title_append, :description_append, :payment_method_append
 
   private
 
@@ -16,5 +19,28 @@ class Item < ApplicationRecord
     return if entry_deadline_at.nil?
 
     self.entry_deadline_at = entry_deadline_at.in_time_zone.end_of_day
+  end
+
+  def price_not_change_when_published
+    if published? && will_save_change_to_price?
+      errors.add(:price, "は出品後に変更できません")
+    end
+  end
+
+  def deadline_later_than_today
+    return if entry_deadline_at.present? && entry_deadline_at.to_date >= Date.current
+    
+    errors.add(:entry_deadline_at, "は本日以降に設定してください")
+  end
+
+  def deadline_later_than_original_when_published
+    if will_save_change_to_entry_deadline_at?
+      new_deadline = entry_deadline_at
+      old_deadline = entry_deadline_at_was
+
+      if published? && new_deadline < old_deadline
+        error.add(:entry_deadline_at, "は元の締切日以降に設定してください")
+      end
+    end
   end
 end
