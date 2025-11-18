8a48534ef3bdd18f73c9fc94ef2efd1fa7d6bed2
Kushizaki <kushimegu288@gmail.com>
2025-11-18 18:23:10 +0900

    ビューの調整

--- app/javascript/controllers/hamburger_controller.js
+++ app/javascript/controllers/hamburger_controller.js
@@ -1,11 +1,11 @@
-import { Controller } from "@hotwired/stimulus"
+import { Controller } from '@hotwired/stimulus';
 
 export default class extends Controller {
-  static targets = ["button", "nav"]
+  static targets = ['button', 'nav'];
 
   toggle() {
-    this.buttonTarget.classList.toggle("bg-gray-100")
-    this.buttonTarget.classList.toggle("rounded")
-    this.navTarget.classList.toggle("hidden")
+    this.buttonTarget.classList.toggle('bg-gray-100');
+    this.buttonTarget.classList.toggle('rounded');
+    this.navTarget.classList.toggle('hidden');
   }
 }
