996113806fbedcbc0c930c54574840ef1d9efeea
Kushizaki <kushimegu288@gmail.com>
2025-11-19 17:52:09 +0900

    フォームのバリデーションの修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,11 +2,16 @@ class Item < ApplicationRecord
   belongs_to :user
   has_many_attached :images
 
-  validates :title, :price, :payment_method, :entry_deadline_at, presence: true, on: :publish
+  validates :title, length: { maximum: 255 }, presence: true, on: :publish
+  validates :price, presence: true, on: :publish
+  validates :payment_method, presence: true, on: :publish
+  validates :entry_deadline_at, presence: true, on: :publish
   validates :images, attached: { message: "を1枚以上選択してください" }, on: :publish
   validates :images, limit: { max: 5 }, content_type: [ "image/png", "image/jpeg" ], size: { less_than: 5.megabytes }
   before_save :set_entry_deadline_at_end_of_day
-  validate :price_not_change_after_published, :deadline_today_or_later, :deadline_not_change_earlier_after_published, on: :publish
+  validate :price_not_change_after_published, on: :publish
+  validate :deadline_today_or_later,  on: :publish
+  validate :deadline_not_change_earlier_after_published, on: :publish
 
   enum :shipping_fee_payer, { buyer: 0, seller: 1 }
   enum :status, { draft: 0, published: 1, sold: 2, closed: 3 }
@@ -28,7 +33,7 @@ class Item < ApplicationRecord
   end
 
   def deadline_today_or_later
-    return if entry_deadline_at.nil? || (entry_deadline_at.present? && entry_deadline_at.to_date >= Date.current)
+    return if entry_deadline_at.nil? || entry_deadline_at.to_date >= Date.current
 
     errors.add(:entry_deadline_at, "は本日以降に設定してください")
   end
