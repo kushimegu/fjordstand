153cf9ed543863bd945abed7b45f6d57d67f0853
Kushizaki <kushimegu288@gmail.com>
2025-11-25 17:01:53 +0900

    締め切った商品に応募できないバリデーションのテストを追加

--- spec/models/entry_spec.rb
+++ spec/models/entry_spec.rb
@@ -1,17 +1,26 @@
 require 'rails_helper'
 
 RSpec.describe Entry, type: :model do
-  describe "cannot_apply_to_own_item" do
-    let(:user) { create(:user, uid: "1234567890") }
-    let(:item) { create(:item, :with_max_five_images, user: user, status: :published) }
+  let(:seller) { create(:user, uid: "1234567890") }
 
-    context "when trying to apply to own item" do
-      let(:entry) { build(:entry, item: item, user: user, status: :applied) }
+  describe "cannot_apply_for_own_item" do
+    let(:item) { create(:item, :with_max_five_images, user: seller, status: :published) }
+    let(:entry) { build(:entry, item: item, user: seller, status: :applied) }
 
-      it "validates applying to own item" do
-        entry.valid?
-        expect(entry.errors.full_messages).to include("自分の出品物には応募できません")
-      end
+    it "validates applying for own item" do
+      entry.valid?
+      expect(entry.errors.full_messages).to include("自分の出品物には応募できません")
+    end
+  end
+
+  describe "cannot_apply_for_expired_item" do
+    let(:item) { create(:item, :with_max_five_images, user: seller, status: :published, entry_deadline_at: Date.yesterday.end_of_day) }
+    let(:buyer) { create(:user, uid: "1234567891") }
+    let(:entry) { build(:entry, item: item, user: buyer, status: :applied) }
+
+    it "validates applying for expired item" do
+      entry.valid?
+      expect(entry.errors.full_messages).to include("締切の過ぎた商品には応募できません")
     end
   end
 end
