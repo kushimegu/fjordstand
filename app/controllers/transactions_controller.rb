d4983ca369e896ba1cf7d1836dae95947c8563d2
Kushizaki <kushimegu288@gmail.com>
2025-11-28 18:11:04 +0900

    連絡ページ一覧に出品者または購入者を表示するよう変更

--- app/controllers/transactions_controller.rb
+++ app/controllers/transactions_controller.rb
@@ -1,6 +1,6 @@
 class TransactionsController < ApplicationController
   def index
-    @items = Item.where(user_id: current_user.id)
+    @items = Item.where(user_id: current_user.id, status: :sold)
                   .or(Item.where(id: Entry.where(user_id: current_user.id, status: :won).select(:item_id)))
   end
 end
