a547cbece36069e41e735cf6892cdecfa9b2b3e0
Kushizaki <kushimegu288@gmail.com>
2025-12-18 15:47:54 +0900

    テストの修正

--- spec/models/notification_spec.rb
+++ spec/models/notification_spec.rb
@@ -1,138 +1,134 @@
 require 'rails_helper'
 
 RSpec.describe Notification, type: :model do
-  let(:seller) { create(:user, uid: "1") }
-  let(:buyer) { create(:user, uid: "2") }
-  let(:loser) { create(:user, uid: "3")}
+  let(:seller) { create(:user) }
+  let(:buyer) { create(:user) }
 
   let!(:sold_item) { create(:item, user: seller, status: :sold) }
-  let!(:closed_item) { create(:item, user: seller, status: :closed)}
-  let!(:entry) { create(:entry, user: buyer, item: sold_item, status: :won)}
-  
-  describe "scopes" do
-    let!(:unread_notification) { create(:notification, :for_item, user: seller, notifiable: sold_item, read: false)}
-    let!(:read_notification) { create(:notification, :for_item, user: seller, notifiable: closed_item, read: true)}
+  let!(:closed_item) { create(:item, user: seller, status: :closed) }
+  let!(:entry) { create(:entry, user: buyer, item: sold_item, status: :won) }
 
-    describe ".unread" do
-      it "returns only unread notification" do
-        result = Notification.unread
+  describe ".unread" do
+    it "returns only unread notification" do
+      unread_notification = create(:notification, :for_item, user: seller, notifiable: sold_item)
+      read_notification = create(:notification, :for_item, user: seller, notifiable: closed_item, read: true)
+      result = described_class.unread
+
+      expect(result).to include(unread_notification)
+      expect(result).not_to include(read_notification)
+    end
+  end
+
+  describe ".by_target" do
+    context "when target is unread" do
+      it "returns unread notifications" do
+        unread_notification = create(:notification, :for_item, user: seller, notifiable: sold_item)
+        read_notification = create(:notification, :for_item, user: seller, notifiable: closed_item, read: true)
+        result = described_class.by_target("unread")
 
         expect(result).to include(unread_notification)
-        expect(result).to_not include(read_notification)
+        expect(result).not_to include(read_notification)
       end
     end
 
-    describe ".by_target" do
-      context "when target is unread" do
-        it "returns unread notifications" do
-          result = Notification.by_target("unread")
+    context "when target is invalid" do
+      it "returns all notifications" do
+        unread_notification = create(:notification, :for_item, user: seller, notifiable: sold_item)
+        read_notification = create(:notification, :for_item, user: seller, notifiable: closed_item, read: true)
+        result = described_class.by_target("invalid_status")
 
-          expect(result).to include(unread_notification)
-          expect(result).to_not include(read_notification)
-        end
-      end
-
-      context "when target is invalid" do
-        it "returns all notifications" do
-          result = Notification.by_target("invalid_status")
-
-          expect(result).to include(unread_notification)
-          expect(result).to include(read_notification)
-        end
+        expect(result).to include(unread_notification)
+        expect(result).to include(read_notification)
       end
     end
   end
 
   describe "#message" do
-    context "when notifiable is Message" do
-      it "returns message notification" do
+    context "when notifiable is message" do
+      it "returns notification message" do
         message = create(:message, user: buyer, item: sold_item)
+        notification = seller.notifications.last
 
-        expect(seller.notifications.last.message).to include("メッセージが届きました")
+        expect(notification.message).to include("メッセージが届きました")
       end
     end
 
-    context "when notifiable is Entry" do
-      context "when entry won" do
-        it "returns won message notification" do
-          notification = create(:notification, :for_entry, notifiable: entry, user: buyer)
+    context "when notifiable is won entry" do
+      it "returns won notification message" do
+        notification = create(:notification, :for_entry, notifiable: entry, user: buyer)
 
-          expect(notification.message).to include("当選しました")
-        end
-      end
-
-      context "when entry lost" do
-        it "returns lost message notification" do
-          lost_entry = create(:entry, user: loser, item: sold_item, status: :lost)
-          notification = create(:notification, :for_entry, notifiable: lost_entry, user: loser)
-
-          expect(notification.message).to include("落選しました")
-        end
+        expect(notification.message).to include("当選しました")
       end
     end
 
-    context "when notifiable is Item" do
-      context "when item was sold" do
-        it "returns sold message notification" do
-          notification = create(:notification, :for_item, notifiable: sold_item, user: seller)
+    context "when notifiable is lost entry" do
+      it "returns lost notification message" do
+        loser = create(:user)
+        lost_entry = create(:entry, user: loser, item: sold_item, status: :lost)
+        notification = create(:notification, :for_entry, notifiable: lost_entry, user: loser)
 
-          expect(notification.message).to include("当選者が決まりました")
-        end
+        expect(notification.message).to include("落選しました")
       end
+    end
 
-      context "when item was closed" do
-        it "returns closed message notification" do
-          notification = create(:notification, :for_item, notifiable: closed_item, user: seller)
+    context "when notifiable is sold item" do
+      it "returns sold notification message" do
+        notification = create(:notification, :for_item, notifiable: sold_item, user: seller)
 
-          expect(notification.message).to include("公開終了しました")
-        end
+        expect(notification.message).to include("当選者が決まりました")
+      end
+    end
+
+    context "when notifiable is closed item" do
+      it "returns closed notification message" do
+        notification = create(:notification, :for_item, notifiable: closed_item, user: seller)
+
+        expect(notification.message).to include("公開終了しました")
       end
     end
   end
 
   describe "#link" do
-    context "when notifiable is Message" do
+    context "when notifiable is message" do
       it "returns link to messages" do
         message = create(:message, user: buyer, item: sold_item)
+        notification = seller.notifications.last
 
-        expect(seller.notifications.last.link).to eq("/transactions/#{sold_item.id}/messages")
+        expect(notification.link).to eq("/transactions/#{sold_item.id}/messages")
       end
     end
 
-    context "when notifiable is Entry" do
-      context "when entry won" do
-        it "returns link to messages" do
-          notification = create(:notification, :for_entry, notifiable: entry, user: buyer)
+    context "when notifiable is won entry" do
+      it "returns link to messages" do
+        notification = create(:notification, :for_entry, notifiable: entry, user: buyer)
 
-          expect(notification.link).to eq("/transactions/#{sold_item.id}/messages")
-        end
-      end
-
-      context "when entry lost" do
-        it "returns link to item" do
-          lost_entry = create(:entry, user: loser, item: sold_item, status: :lost)
-          notification = create(:notification, :for_entry, notifiable: lost_entry, user: loser)
-          
-          expect(notification.link).to eq("/items/#{sold_item.id}")
-        end
+        expect(notification.link).to eq("/transactions/#{sold_item.id}/messages")
       end
     end
-    
-    context "when notifiable is Item" do
-      context "when item is sold" do
-        it "returns link to message" do
-          notification = create(:notification, :for_item, notifiable: sold_item, user: seller)
 
-          expect(notification.link).to eq("/transactions/#{sold_item.id}/messages")
-        end
+    context "when notifiable is lost entry" do
+      it "returns link to item" do
+        loser = create(:user, uid: "3")
+        lost_entry = create(:entry, user: loser, item: sold_item, status: :lost)
+        notification = create(:notification, :for_entry, notifiable: lost_entry, user: loser)
+
+        expect(notification.link).to eq("/items/#{sold_item.id}")
       end
+    end
 
-      context "when item is closed" do
-        it "returns link to item" do
-          notification = create(:notification, :for_item, notifiable: closed_item, user: seller)
+    context "when notifiable is sold item" do
+      it "returns link to message" do
+        notification = create(:notification, :for_item, notifiable: sold_item, user: seller)
 
-          expect(notification.link).to eq("/items/#{closed_item.id}")
-        end
+        expect(notification.link).to eq("/transactions/#{sold_item.id}/messages")
+      end
+    end
+
+    context "when notifiable is closed item" do
+      it "returns link to item" do
+        notification = create(:notification, :for_item, notifiable: closed_item, user: seller)
+
+        expect(notification.link).to eq("/items/#{closed_item.id}")
       end
     end
   end
--- spec/requests/notifications_spec.rb
+++ spec/requests/notifications_spec.rb
@@ -1,11 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe "Notifications", type: :request do
-  let(:seller) { create(:user, uid: "1234567890") }
-  let(:buyer) { create(:user, uid: "1234567891") }
+  let(:seller) { create(:user) }
+  let(:buyer) { create(:user) }
 
   let!(:sold_item) { create(:item, user: seller, status: :sold) }
-  let!(:closed_item) { create(:item, user: seller, status: :closed)}
+  let!(:closed_item) { create(:item, user: seller, status: :closed) }
   let!(:entry) { create(:entry, user: buyer, item: sold_item, status: :won) }
 
   before do
@@ -14,17 +14,17 @@ RSpec.describe "Notifications", type: :request do
 
   describe "GET /index" do
     context "when notifications exists" do
-      let!(:unread_notification) { create(:notification, :for_item, user: seller, notifiable: sold_item, read: false)}
-      let!(:read_notification) { create(:notification, :for_item, user: seller, notifiable: closed_item, read: true)}
-      let!(:other_notification) { create(:notification, :for_entry, notifiable: entry, user: buyer)}
-    
       it "returns current users notifications with http success" do
+        unread_notification = create(:notification, :for_item, user: seller, notifiable: sold_item)
+        read_notification = create(:notification, :for_item, user: seller, notifiable: closed_item, read: true)
+        other_notification = create(:notification, :for_entry, notifiable: entry, user: buyer)
+
         get notifications_path
         expect(response).to have_http_status(:success)
 
         expect(response.body).to include(unread_notification.message)
         expect(response.body).to include(read_notification.message)
-        expect(response.body).to_not include(other_notification.message)
+        expect(response.body).not_to include(other_notification.message)
       end
     end
 
@@ -36,42 +36,42 @@ RSpec.describe "Notifications", type: :request do
         expect(response.body).to include("通知はありません")
       end
     end
-  
-    context "when filtering by status" do
-      let!(:unread_notification) { create(:notification, :for_item, user: seller, notifiable: sold_item, read: false)}
-      let!(:read_notification) { create(:notification, :for_item, user: seller, notifiable: closed_item, read: true)}
 
-      context "when status is unread" do
-        it "returns unread notifications" do
-          get notifications_path(status: "unread")
-          expect(response).to have_http_status(:success)
+    context "when filtering by unread status" do
+      it "returns unread notifications" do
+        unread_notification = create(:notification, :for_item, user: seller, notifiable: sold_item)
+        read_notification = create(:notification, :for_item, user: seller, notifiable: closed_item, read: true)
 
-          expect(response.body).to include(unread_notification.message)
-          expect(response.body).to_not include(read_notification.message)
-        end
+        get notifications_path(status: "unread")
+        expect(response).to have_http_status(:success)
+
+        expect(response.body).to include(unread_notification.message)
+        expect(response.body).not_to include(read_notification.message)
       end
+    end
 
-      context "when status is invalid" do
-        it "returns all notifications" do
-          get notifications_path(status: "invalid_status")
+    context "when filtering by invalid status" do
+      it "returns all notifications" do
+        unread_notification = create(:notification, :for_item, user: seller, notifiable: sold_item)
+        read_notification = create(:notification, :for_item, user: seller, notifiable: closed_item, read: true)
 
-          expect(response).to have_http_status(:success)
+        get notifications_path(status: "invalid_status")
+        expect(response).to have_http_status(:success)
 
-          expect(response.body).to include(unread_notification.message)
-          expect(response.body).to include(read_notification.message)
-        end
+        expect(response.body).to include(unread_notification.message)
+        expect(response.body).to include(read_notification.message)
       end
     end
   end
 
-  describe "GET /read" do
-    let!(:unread_notification1) { create(:notification, :for_item, user: seller, notifiable: sold_item, read: false)}
-    let!(:unread_notification2) { create(:notification, :for_item, user: seller, notifiable: closed_item, read: false)}
-
+  describe "PATCH /read" do
     it "updates read status to true and redirect to notification link" do
+      unread_notification1 = create(:notification, :for_item, user: seller, notifiable: sold_item)
+      unread_notification2 = create(:notification, :for_item, user: seller, notifiable: closed_item)
+
       expect(unread_notification1.read).to be false
       expect(unread_notification2.read).to be false
-      get read_notification_path(unread_notification1)
+      patch read_notification_path(unread_notification1)
 
       expect(unread_notification1.reload.read).to be true
       expect(unread_notification2.reload.read).to be false
@@ -79,14 +79,14 @@ RSpec.describe "Notifications", type: :request do
     end
   end
 
-  describe "GET /read_all" do
-    let!(:unread_notification1) { create(:notification, :for_item, user: seller, notifiable: sold_item, read: false)}
-    let!(:unread_notification2) { create(:notification, :for_item, user: seller, notifiable: closed_item, read: false)}
-    
+  describe "PATCH /read_all" do
     it "updates all notifications status to read and redirect to notifications path" do
+      unread_notification1 = create(:notification, :for_item, user: seller, notifiable: sold_item)
+      unread_notification2 = create(:notification, :for_item, user: seller, notifiable: closed_item)
+
       expect(unread_notification1.read).to be false
       expect(unread_notification2.read).to be false
-      get read_all_notifications_path
+      patch read_all_notifications_path
 
       expect(unread_notification1.reload.read).to be true
       expect(unread_notification2.reload.read).to be true
