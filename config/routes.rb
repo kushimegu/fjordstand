3bbae703f6bd61033d2f2374cbecc5d2692c94c1
Kushizaki <kushimegu288@gmail.com>
2025-12-19 15:18:46 +0900

    logoutをdelete操作に変更

--- config/routes.rb
+++ config/routes.rb
@@ -16,5 +16,5 @@ Rails.application.routes.draw do
   resource :session, only: %i[create destroy]
   get 'auth/:provider/callback', to: 'sessions#create'
   get 'auth/failure', to: redirect('/')
-  get 'logout', to: 'sessions#destroy', as: 'logout'
+  delete 'logout', to: 'sessions#destroy', as: 'logout'
 end
