27203ff294df94fbabc37efa7ca4ef29ac9a65c1
Kushizaki <kushimegu288@gmail.com>
2025-12-25 15:37:43 +0900

    コメント機能のテストを追加

---
+++ spec/requests/comments_spec.rb
@@ -0,0 +1,44 @@
+require 'rails_helper'
+
+RSpec.describe "/comments", type: :request do
+  let(:user) { create(:user) }
+  let(:item) { create(:item, user: user, status: :published) }
+
+  let(:valid_attributes) { {
+    body: "有効なコメント"
+  } }
+
+  let(:invalid_attributes) { {
+    body: ""
+  } }
+
+  before { login(user) }
+
+  describe "POST /create" do
+    context "with valid parameters" do
+      it "creates a new Comment" do
+        expect {
+          post item_comments_path(item), params: { comment: valid_attributes }
+        }.to change(Comment, :count).by(1)
+      end
+
+      it "redirects to the item" do
+        post item_comments_path(item), params: { comment: valid_attributes }
+        expect(response).to redirect_to(item_path(item))
+      end
+    end
+
+    context "with invalid parameters" do
+      it "does not create a new Comment" do
+        expect {
+          post item_comments_path(item), params: { comment: invalid_attributes }
+        }.to change(Comment, :count).by(0)
+      end
+
+      it "renders a response with 422 status (i.e. to display the 'new' template)" do
+        post item_comments_path(item), params: { comment: invalid_attributes }
+        expect(response).to have_http_status(:unprocessable_content)
+      end
+    end
+  end
+end
