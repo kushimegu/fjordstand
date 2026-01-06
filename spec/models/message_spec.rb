5c05af9f85a1b5ca9517ff593c79d3eeae32fca5
Kushizaki <kushimegu288@gmail.com>
2026-01-06 16:15:49 +0900

    メッセージ関連のテストを追加

--- spec/models/message_spec.rb
+++ spec/models/message_spec.rb
@@ -1,5 +1,23 @@
 require 'rails_helper'
 
 RSpec.describe Message, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "#create_notifications" do
+    let(:seller) { create(:user) }
+    let(:buyer) { create(:user) }
+    let!(:item) { create(:item, :with_max_five_images, :sold, user: seller) }
+
+    context "when sender is seller" do
+      it "creates notification to buyer" do
+        create(:entry, :won, item: item, user: buyer)
+        expect { create(:message, item: item, user: seller) }.to change { buyer.notifications.count }.by(1)
+      end
+    end
+
+    context "when sender is buyer" do
+      it "creates notification to seller" do
+        create(:entry, :won, item: item, user: buyer)
+        expect { create(:message, item: item, user: buyer) }.to change { seller.notifications.count }.by(1)
+      end
+    end
+  end
 end
