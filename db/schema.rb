874bca00b300c593d8a53e16028581b596c0e848
Kushizaki <kushimegu288@gmail.com>
2026-03-03 16:56:32 +0900

    Userにadminを追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_03_02_085242) do
+ActiveRecord::Schema[8.1].define(version: 2026_03_03_073346) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -99,6 +99,7 @@ ActiveRecord::Schema[8.1].define(version: 2026_03_02_085242) do
   end
 
   create_table "users", force: :cascade do |t|
+    t.boolean "admin", default: false
     t.string "avatar_url"
     t.datetime "created_at", null: false
     t.string "name", null: false
