4805030f665b4d42a42aa9ce83f9da40dcd7b224
Kushizaki <kushimegu288@gmail.com>
2026-03-24 20:44:57 +0900

    omnitauth設定の修正

--- config/initializers/omiauth.rb
+++ config/initializers/omiauth.rb
@@ -1,5 +1,5 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :discord, ENV['DISCORD_CLIENT_ID'], ENV['DISCORD_CLIENT_SECRET'], callback_url: ENV["DISCORD_REDIRECT_URI"]
+  provider :discord, ENV['DISCORD_CLIENT_ID'], ENV['DISCORD_CLIENT_SECRET'], callback_url: ENV["DISCORD_REDIRECT_URI"], provider_ignores_state: true
 end
 
 OmniAuth.config.on_failure = Proc.new { |env|
