b7ce77482d098dd575b06aceb10ef1d98fbfe064
Kushizaki <kushimegu288@gmail.com>
2026-03-25 15:05:58 +0900

    faviconの修正

--- config/routes.rb
+++ config/routes.rb
@@ -23,4 +23,6 @@ Rails.application.routes.draw do
   get 'auth/failure', to: redirect('/')
   delete 'logout', to: 'sessions#destroy', as: 'logout'
   get 'up' => 'rails/health#show', as: :rails_health_check
+  get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
+  get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
 end
