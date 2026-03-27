b1f0cae20132e6a592a639e022d4ab46a04d315b
Kushizaki <kushimegu288@gmail.com>
2026-03-27 10:46:09 +0900

    本番環境でのcronの設定

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,10 +1,15 @@
 require File.expand_path(File.dirname(__FILE__) + "/environment")
 rails_env = ENV['RAILS_ENV'] || :development
 set :environment, rails_env
-set :output, "#{Rails.root}/log/crontab.log"
 
-job_type :rake, "export PATH=\"$HOME/.rbenv/bin:$HOME/.rbenv/shims:$PATH\"; eval \"$(rbenv init -)\"; cd :path && RAILS_ENV=:environment bundle exec rake :task --silent :output"
-set :job_template, "/bin/zsh -c ':job'"
+if rails_env == :production
+  set :output, { standard: '/proc/1/fd/1', error: '/proc/1/fd/2' }
+  job_type :rake, "/rails/bin/cron_executor bundle exec rake :task :output"
+else
+  set :output, "#{Rails.root}/log/crontab.log"
+  job_type :rake, "export PATH=\"$HOME/.rbenv/bin:$HOME/.rbenv/shims:$PATH\"; eval \"$(rbenv init -)\"; cd :path && RAILS_ENV=:environment bundle exec rake :task --silent :output"
+  set :job_template, "/bin/zsh -c ':job'"
+end
 
 every 1.day, at: '8:00 am' do
   rake "lottery:select_winner"
