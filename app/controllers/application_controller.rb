f7ca4c444946b7bf33bcfec9f5deae4abd0ec4e2
Kushizaki <kushimegu288@gmail.com>
2026-03-31 13:27:36 +0900

    デザインレビューの準備

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < ActionController::Base
   include SessionsHelper
 
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
-  allow_browser versions: :modern
+  # allow_browser versions: :modern
   before_action :check_logged_in
 
   def check_logged_in
