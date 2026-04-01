862872994202a4953514726c4fe10026e7605382
Kushizaki <kushimegu288@gmail.com>
2026-04-01 12:13:24 +0900

    bashのパスを修正

--- config/schedule.rb
+++ config/schedule.rb
@@ -5,7 +5,7 @@ set :environment, rails_env
 if rails_env == :production
   set :output, { standard: '/proc/1/fd/1', error: '/proc/1/fd/2' }
   job_type :rake, "/rails/bin/cron_executor bundle exec rake :task :output"
-  set :job_template, "/bin/bash -l -c ':job'"
+  set :job_template, "/usr/bin/bash -l -c ':job'"
 else
   set :output, "#{Rails.root}/log/crontab.log"
   job_type :rake, "export PATH=\"$HOME/.rbenv/bin:$HOME/.rbenv/shims:$PATH\"; eval \"$(rbenv init -)\"; cd :path && RAILS_ENV=:environment bundle exec rake :task --silent :output"
