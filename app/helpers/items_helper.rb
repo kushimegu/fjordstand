6ab8da7a88dd329fcbcc4ed5424283ed7dcbc5c7
Kushizaki <kushimegu288@gmail.com>
2026-04-14 10:47:56 +0900

    linterに沿った修正

--- app/helpers/items_helper.rb
+++ app/helpers/items_helper.rb
@@ -1,10 +1,10 @@
 module ItemsHelper
   def shipping_fee_class(payer)
     case payer
-    when 'seller'
-      'text-red-400'
+    when "seller"
+      "text-red-400"
     else
-      'text-gray-600'
+      "text-gray-600"
     end
   end
 end
