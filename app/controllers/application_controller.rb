02c5c6fa08fe4555cceb8041442df441eebf8105
Kushizaki <kushimegu288@gmail.com>
2026-04-17 16:00:08 +0900

    連絡タブに未読通知を追加、メッセージを開くと既読に

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,10 +4,17 @@ class ApplicationController < ActionController::Base
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   # allow_browser versions: :modern
   before_action :check_logged_in
+  before_action :preload_current_user_notifications, if: :logged_in?
+
+  private
 
   def check_logged_in
     return if current_user
 
     redirect_to root_path
+  end
+
+  def preload_current_user_notifications
+    current_user.notifications.unread.load
   end
 end
