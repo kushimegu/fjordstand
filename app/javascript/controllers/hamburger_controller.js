2c85bba73c7643aadd33a5102d88f864535889c4
Kushizaki <kushimegu288@gmail.com>
2026-04-23 15:41:50 +0900

    デザイン調整に伴うjsファイルの修正

---
+++ app/javascript/controllers/hamburger_controller.js
@@ -0,0 +1,11 @@
+import { Controller } from '@hotwired/stimulus';
+
+export default class extends Controller {
+  static targets = ['button', 'nav'];
+
+  toggle() {
+    this.buttonTarget.classList.toggle('bg-gray-100');
+    this.buttonTarget.classList.toggle('rounded');
+    this.navTarget.classList.toggle('hidden');
+  }
+}
