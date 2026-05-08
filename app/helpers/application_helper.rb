769ba0bf3a27958190cf364d4d7b804fc6d49931
Kushizaki <kushimegu288@gmail.com>
2026-05-08 11:36:44 +0900

    商品詳細から戻るリンクの修正

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,4 +4,23 @@ module ApplicationHelper
 
     page_title.empty? ? base_title : "#{page_title} | #{base_title}"
   end
+
+  def back_link_for_item(item)
+    case params[:from]
+    when "watches"
+      label, path = "Watch中一覧へ", watches_path
+    when "entries"
+      label, path = "希望商品一覧へ", entries_path
+    when "listings"
+      label, path = "自分の出品へ", listings_path
+    when "messages"
+      label, path = "連絡ページへ", transaction_messages_path(item)
+    else
+      label, path = "販売中商品へ", items_path
+    end
+
+    link_to path, class: "mt-4 flex items-center justify-center text-sm text-gray-500 hover:text-gray-700 underline" do
+      label
+    end
+  end
 end
