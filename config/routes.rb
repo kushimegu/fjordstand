5fcc81cc879abfb001c93be670489254aa7d8a25
Kushizaki <kushimegu288@gmail.com>
2026-05-15 18:19:36 +0900

    ルーティングの記述順変更

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
+  resources :items do
+    resource :entries, only: %i[create destroy]
+    resources :comments, only: %i[create destroy]
+    resource :watches, only: %i[create destroy]
+  end
+  resources :transactions, only: [:index] do
+    resources :messages, only: %i[index create destroy]
+  end
   resources :notifications, only: [:index] do
     scope module: :notifications do
       resource :read, only: %i[update]
@@ -7,26 +15,18 @@ Rails.application.routes.draw do
       end
     end
   end
-  resources :transactions, only: [:index] do
-    resources :messages, only: %i[index create destroy]
-  end
   resources :entries, only: %i[index]
   scope module: :items do
     resources :listings, only: %i[index]
   end
   resources :watches, only: %i[index]
-  resources :items do
-    resource :entries, only: %i[create destroy]
-    resources :comments, only: %i[create destroy]
-    resource :watches, only: %i[create destroy]
-  end
-  root to: "pages#home"
-  get "/terms", to: "pages#terms"
-  get "/privacy", to: "pages#privacy"
   resource :session, only: %i[create destroy]
   get "auth/:provider/callback", to: "sessions#create"
   get "auth/failure", to: "sessions#failure"
   delete "logout", to: "sessions#destroy", as: "logout"
+  get "/terms", to: "pages#terms"
+  get "/privacy", to: "pages#privacy"
+  root to: "pages#home"
   get "up" => "rails/health#show", as: :rails_health_check
   get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
   get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
