14fca449040d6567b07742a9eb9633029cf379d9
Kushizaki <kushimegu288@gmail.com>
2026-05-22 20:05:29 +0900

    インデントの修正

--- app/controllers/items/listings_controller.rb
+++ app/controllers/items/listings_controller.rb
@@ -1,9 +1,9 @@
 class Items::ListingsController < ApplicationController
   def index
     listings_scope = current_user.items
-                              .includes(:winner, images_attachments: :blob)
-                              .by_target(params[:status])
-                              .order(entry_deadline_at: :desc, updated_at: :desc)
+                                  .includes(:winner, images_attachments: :blob)
+                                  .by_target(params[:status])
+                                  .order(entry_deadline_at: :desc, updated_at: :desc)
     @my_entries  = current_user.entries.where(item: listings_scope).index_by(&:item_id)
     @my_watches  = current_user.watches.where(item: listings_scope).pluck(:item_id).to_set
     @listings = listings_scope.page(params[:page]).per(16)
