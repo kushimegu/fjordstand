88a2e87bacad8825a09f81096697ce7fb0c0eb89
Kushizaki <kushimegu288@gmail.com>
2026-06-01 17:03:54 +0900

    通知一覧のN＋1懸念の解消

--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -1,7 +1,7 @@
 class NotificationsController < ApplicationController
   def index
     @notifications = current_user.notifications
-                                  .includes(:notifiable)
+                                  .preload(notifiable: [:user, { item: [:user, :winner] }])
                                   .by_target(params[:status])
                                   .order(created_at: :desc)
                                   .page(params[:page])
