58101b59ec7c1d6b56717c66bc188b3e5557055a
Kushizaki <kushimegu288@gmail.com>
2026-06-03 18:21:59 +0900

    バリデーションで弾かれたときリダイレクトされるように修正

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,8 +8,10 @@ class SessionsController < ApplicationController
       reset_session
       session[:user_id] = user.id
       redirect_to items_path, notice: "ログインしました"
-    else
+    elsif user == :not_member
       redirect_to root_path, alert: "FjordBootCampのDiscordサーバーに参加していません"
+    else
+      redirect_to root_path, alert: "ログインに失敗しました"
     end
   end
 
