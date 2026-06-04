81d016be4d50762b108483f020273d34b7aca0d2
Kushizaki <kushimegu288@gmail.com>
2026-06-04 16:10:23 +0900

    SessionsHelperを廃止しコントローラに移動、ユーザーnilに対応

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,20 @@
 class ApplicationController < ActionController::Base
-  include SessionsHelper
+  helper_method :current_user, :logged_in?
 
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   # allow_browser versions: :modern
   before_action :check_logged_in
   before_action :preload_current_user_notifications, if: :logged_in?
+
+  def current_user
+    return unless (user_id = session[:user_id])
+
+    defined?(@current_user) ? @current_user : @current_user = User.find_by(id: user_id)
+  end
+
+  def logged_in?
+    !current_user.nil?
+  end
 
   private
 
