4cbf8637dae447aed3873d8772ea645214f5d99e
Kushizaki <kushimegu288@gmail.com>
2026-06-09 16:16:10 +0900

    誤って変更したlistingsの表示順を戻した

--- app/controllers/items/listings_controller.rb
+++ app/controllers/items/listings_controller.rb
@@ -2,7 +2,7 @@ class Items::ListingsController < ApplicationController
   def index
     @listings = current_user.items
                             .by_target(params[:status])
-                            .order(entry_deadline_at: :desc, created_at: :desc)
+                            .order(entry_deadline_at: :desc, updated_at: :desc)
                             .includes(:winner, images_attachments: :blob)
                             .page(params[:page])
                             .per(16)
