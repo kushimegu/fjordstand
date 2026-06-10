bf74afd1d80c9bd46643458c12016dd02eeea3fc
Kushizaki <kushimegu288@gmail.com>
2026-06-10 16:12:07 +0900

    既読のアクション名とURLを変更

--- app/controllers/notifications/reads_controller.rb
+++ app/controllers/notifications/reads_controller.rb
@@ -1,16 +1,16 @@
 class Notifications::ReadsController < ApplicationController
-  def update
-    notification = current_user.notifications.find(params[:notification_id])
+  def mark_as_read
+    notification = current_user.notifications.find(params[:id])
     if notification.notifiable_type == "Comment"
       comment = notification.notifiable
       Notification.update_all_read_by_ids!(current_user, "Comment", comment.item.comment_ids)
     else
-      notification.update(read: true)
+      notification.update!(read: true)
     end
     redirect_to url_for("#{notification.link}?from=notifications"), status: :see_other
   end
 
-  def update_all
+  def mark_all_as_read
     current_user.notifications.unread.update_all(read: true)
     redirect_to notifications_path, notice: "全て既読にしました", status: :see_other
   end
