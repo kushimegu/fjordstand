ca81978bff9d3c6107183147da9e405ffcc239db
Kushizaki <kushimegu288@gmail.com>
2026-06-12 16:59:42 +0900

    find_byを使うアクションで成功時のみフラッシュを表示するよう修正

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -23,10 +23,12 @@ class EntriesController < ApplicationController
 
   # DELETE /entries/1
   def destroy
-    entry = current_user.entries.find_by!(item_id: @item.id)
+    entry = current_user.entries.find_by(item_id: @item.id)
 
-    entry.destroy!
-    redirect_to @item, notice: "購入希望を取り消しました", status: :see_other
+    if entry&.destroy!
+      flash[:notice] = "購入希望を取り消しました"
+    end
+    redirect_to @item, status: :see_other
   end
 
   private
