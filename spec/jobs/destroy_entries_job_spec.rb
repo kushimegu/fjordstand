9c51de649aa87d38b066d711c00a780f535acd2a
Kushizaki <kushimegu288@gmail.com>
2026-06-18 18:21:17 +0900

    job specの修正

--- spec/jobs/destroy_entries_job_spec.rb
+++ spec/jobs/destroy_entries_job_spec.rb
@@ -3,11 +3,14 @@ require 'rails_helper'
 RSpec.describe DestroyEntriesJob, type: :job do
   let(:seller) { create(:user) }
   let(:item) { create(:item, :published, user: seller) }
-  let(:buyer) { create(:user) }
+  let(:applicant) { create(:user) }
+  let(:other_applicant) { create(:user) }
 
   before do
     ActiveJob::Base.queue_adapter = :test
-    create(:entry, item: item, user: buyer)
+
+    create(:entry, item: item, user: applicant)
+    create(:entry, item: item, user: other_applicant)
   end
 
   describe '#perform_later' do
@@ -16,8 +19,8 @@ RSpec.describe DestroyEntriesJob, type: :job do
       expect(DestroyEntriesJob).to have_been_enqueued.with(item.id)
     end
 
-    it 'destroys the entry' do
-      expect { DestroyEntriesJob.perform_now(item.id) }.to change(Entry, :count).by(-1)
+    it 'enqueues DestroyEntriesJob' do
+      expect { DestroyEntriesJob.perform_now(item.id) }.to change(Entry, :count).by(-2)
     end
   end
 end
