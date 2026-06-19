495c9ef210bec799fdb812640d124b7d640364d5
Kushizaki <kushimegu288@gmail.com>
2026-06-19 11:45:32 +0900

    グローバルナビの強調表示のテストの追加・修正

--- spec/helpers/application_helper_spec.rb
+++ spec/helpers/application_helper_spec.rb
@@ -2,6 +2,13 @@ require 'rails_helper'
 
 RSpec.describe ApplicationHelper, type: :helper do
   let(:item) { create(:item, :published) }
+  let(:user) { create(:user) }
+
+  before do
+    without_partial_double_verification do
+      allow(helper).to receive(:current_user).and_return(user)
+    end
+  end
 
   describe "#page_title" do
     context "when page title is empty" do
@@ -20,9 +27,7 @@ RSpec.describe ApplicationHelper, type: :helper do
 
   describe "#back_link_for_item" do
     context "when from watches" do
-      before do
-        controller.params = { from: "watches" }
-      end
+      before { controller.params = { from: "watches" } }
 
       it "returns link to watches" do
         expect(helper.back_link_for_item(item)).to include("Watch中一覧へ", watches_path)
@@ -30,9 +35,7 @@ RSpec.describe ApplicationHelper, type: :helper do
     end
 
     context "when from entries" do
-      before do
-        controller.params = { from: "entries" }
-      end
+      before { controller.params = { from: "entries" } }
 
       it "returns link to entries" do
         expect(helper.back_link_for_item(item)).to include("希望商品一覧へ", entries_path)
@@ -40,9 +43,7 @@ RSpec.describe ApplicationHelper, type: :helper do
     end
 
     context "when from listings" do
-      before do
-        controller.params = { from: "listings" }
-      end
+      before { controller.params = { from: "listings" } }
 
       it "returns link to listings" do
         expect(helper.back_link_for_item(item)).to include("自分の出品一覧へ", listings_path)
@@ -50,9 +51,7 @@ RSpec.describe ApplicationHelper, type: :helper do
     end
 
     context "when from messages" do
-      before do
-        controller.params = { from: "messages" }
-      end
+      before { controller.params = { from: "messages" } }
 
       it "returns link to messages" do
         expect(helper.back_link_for_item(item)).to include("連絡ページへ", conversation_messages_path(item))
@@ -79,9 +78,7 @@ RSpec.describe ApplicationHelper, type: :helper do
     end
 
     context "when current page is items_path" do
-      before do
-        allow(helper).to receive(:current_page?) { |path| path == items_path }
-      end
+      before { allow(helper).to receive(:current_page?) { |path| path == items_path } }
 
       it "returns true" do
         expect(helper.active_items_tab?).to be true
@@ -89,9 +86,7 @@ RSpec.describe ApplicationHelper, type: :helper do
     end
 
     context "when current page is watches_path" do
-      before do
-        allow(helper).to receive(:current_page?) { |path| path == watches_path }
-      end
+      before { allow(helper).to receive(:current_page?) { |path| path == watches_path } }
 
       it "returns true" do
         expect(helper.active_items_tab?).to be true
@@ -99,9 +94,7 @@ RSpec.describe ApplicationHelper, type: :helper do
     end
 
     context "when fullpath includes from=watches" do
-      before do
-        controller.params = { from: "watches" }
-      end
+      before { controller.params = { from: "watches" } }
 
       it "returns true" do
         expect(helper.active_items_tab?).to be true
@@ -109,9 +102,7 @@ RSpec.describe ApplicationHelper, type: :helper do
     end
 
     context "when fullpath includes from=items" do
-      before do
-        controller.params = { from: "items" }
-      end
+      before { controller.params = { from: "items" } }
 
       it "returns true" do
         expect(helper.active_items_tab?).to be true
@@ -119,9 +110,7 @@ RSpec.describe ApplicationHelper, type: :helper do
     end
 
     context "when fullpath includes from=notifications" do
-      before do
-        controller.params = { from: "notifications" }
-      end
+      before { controller.params = { from: "notifications" } }
 
       it "returns true" do
         expect(helper.active_items_tab?).to be true
@@ -129,9 +118,7 @@ RSpec.describe ApplicationHelper, type: :helper do
     end
 
     context "when current page is other path and fullpath does not include from param" do
-      before do
-        allow(helper).to receive(:current_page?) { |path| path == item_path(item) }
-      end
+      before { allow(helper).to receive(:current_page?) { |path| path == item_path(item) } }
 
       it "returns false" do
         expect(helper.active_items_tab?).to be false
@@ -141,9 +128,7 @@ RSpec.describe ApplicationHelper, type: :helper do
 
   describe "#active_entries_tab?" do
     context "when current page is entries_path" do
-      before do
-        allow(helper).to receive(:current_page?) { |path| path == entries_path }
-      end
+      before { allow(helper).to receive(:current_page?) { |path| path == entries_path } }
 
       it "returns true" do
         expect(helper.active_entries_tab?).to be true
@@ -151,9 +136,7 @@ RSpec.describe ApplicationHelper, type: :helper do
     end
 
     context "when fullpath includes from=entries" do
-      before do
-        controller.params = { from: "entries" }
-      end
+      before { controller.params = { from: "entries" } }
 
       it "returns true" do
         expect(helper.active_entries_tab?).to be true
@@ -161,9 +144,7 @@ RSpec.describe ApplicationHelper, type: :helper do
     end
 
     context "when current page is other path and fullpath does not include from=entries" do
-      before do
-        allow(helper).to receive(:current_page?) { |path| path == item_path(item) }
-      end
+      before { allow(helper).to receive(:current_page?) { |path| path == item_path(item) } }
 
       it "returns false" do
         expect(helper.active_entries_tab?).to be false
@@ -172,10 +153,46 @@ RSpec.describe ApplicationHelper, type: :helper do
   end
 
   describe "#active_listings_tab?" do
-    context "when current page is new_item_path" do
+    let(:item) { create(:item, user: user) }
+
+    context "when page is items show and is not from messages" do
       before do
-        allow(helper).to receive(:current_page?) { |path| path == new_item_path }
+        allow(helper).to receive(:controller_name).and_return("items")
+        allow(helper).to receive(:action_name).and_return("show")
+        controller.params = { from: "watches" }
+        assign(:item, item)
       end
+
+      it "returns true" do
+        expect(helper.active_listings_tab?).to be true
+      end
+    end
+
+    context "when page is items edit" do
+      before do
+        allow(helper).to receive(:controller_name).and_return("items")
+        allow(helper).to receive(:action_name).and_return("edit")
+        assign(:item, item)
+      end
+
+      it "returns true" do
+        expect(helper.active_listings_tab?).to be true
+      end
+    end
+
+    context "when page is items show and is from messages" do
+      before do
+        assign(:item, item)
+        allow(controller.request).to receive(:fullpath).and_return("/items#{item.id}?from=messages")
+      end
+
+      it "returns false" do
+        expect(helper.active_listings_tab?).to be false
+      end
+    end
+
+    context "when current page is new_item_path" do
+      before { allow(helper).to receive(:current_page?) { |path| path == new_item_path } }
 
       it "returns true" do
         expect(helper.active_listings_tab?).to be true
@@ -183,19 +200,7 @@ RSpec.describe ApplicationHelper, type: :helper do
     end
 
     context "when current page is listings_path" do
-      before do
-        allow(helper).to receive(:current_page?) { |path| path == listings_path }
-      end
-
-      it "returns true" do
-        expect(helper.active_listings_tab?).to be true
-      end
-    end
-
-    context "when current page is edit item path" do
-      before do
-        allow(controller.request).to receive(:path).and_return("/item/#{item.id}/edit")
-      end
+      before { allow(helper).to receive(:current_page?) { |path| path == listings_path } }
 
       it "returns true" do
         expect(helper.active_listings_tab?).to be true
@@ -203,9 +208,7 @@ RSpec.describe ApplicationHelper, type: :helper do
     end
 
     context "when fullpath includes from=listings" do
-      before do
-        controller.params = { from: "listings" }
-      end
+      before { controller.params = { from: "listings" } }
 
       it "returns true" do
         expect(helper.active_listings_tab?).to be true
@@ -213,9 +216,7 @@ RSpec.describe ApplicationHelper, type: :helper do
     end
 
     context "when current page is other path and fullpath does not include from=listings" do
-      before do
-        allow(helper).to receive(:current_page?) { |path| path == item_path(item) }
-      end
+      before { allow(helper).to receive(:current_page?) { |path| path == entries_path } }
 
       it "returns false" do
         expect(helper.active_listings_tab?).to be false
@@ -225,9 +226,7 @@ RSpec.describe ApplicationHelper, type: :helper do
 
   describe "#active_conversations_tab?" do
     context "when current page is conversations_path" do
-      before do
-        allow(controller.request).to receive(:path).and_return("/conversations")
-      end
+      before { allow(controller.request).to receive(:path).and_return("/conversations") }
 
       it "returns true" do
         expect(helper.active_conversations_tab?).to be true
@@ -235,9 +234,7 @@ RSpec.describe ApplicationHelper, type: :helper do
     end
 
     context "when fullpath includes from=messages" do
-      before do
-        controller.params = { from: "messages" }
-      end
+      before { controller.params = { from: "messages" } }
 
       it "returns true" do
         expect(helper.active_conversations_tab?).to be true
@@ -245,9 +242,7 @@ RSpec.describe ApplicationHelper, type: :helper do
     end
 
     context "when current page is other path and fullpath does not include from=messages" do
-      before do
-        allow(controller.request).to receive(:path).and_return("/item/#{item.id}")
-      end
+      before { allow(controller.request).to receive(:path).and_return("/item/#{item.id}") }
 
       it "returns false" do
         expect(helper.active_conversations_tab?).to be false
