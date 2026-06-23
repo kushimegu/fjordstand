c659a66c0b24ab7a3a9918caf6ea18d00430522f
Kushizaki <kushimegu288@gmail.com>
2026-06-23 14:16:24 +0900

    表示件数のper数字を定数定義するよう変更

--- app/controllers/conversations_controller.rb
+++ app/controllers/conversations_controller.rb
@@ -1,9 +1,12 @@
 class ConversationsController < ApplicationController
+
+  PER_PAGE = 10
+
   def index
     @items = current_user.dealing_items
                           .order(updated_at: :desc)
                           .includes(:user, :winner, first_image_attachment: { blob: :variant_records }, messages: :notifications, notifications: :notifiable)
                           .page(params[:page])
-                          .per(10)
+                          .per(PER_PAGE)
   end
 end
--- app/controllers/items/listings_controller.rb
+++ app/controllers/items/listings_controller.rb
@@ -1,10 +1,13 @@
 class Items::ListingsController < ApplicationController
+
+  PER_PAGE = 16
+
   def index
     @listings = current_user.items
                             .by_target(params[:status])
                             .order(entry_deadline_at: :desc, updated_at: :desc)
                             .includes(:winner, first_image_attachment: { blob: :variant_records })
                             .page(params[:page])
-                            .per(16)
+                            .per(PER_PAGE)
   end
 end
