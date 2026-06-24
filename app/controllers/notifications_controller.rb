41f96a1b6074b8fab1c01a5de5247a181847fb46
Kushizaki <kushimegu288@gmail.com>
2026-06-24 15:27:20 +0900

    scopeを使わずパラメータに応じて情報を表示するよう修正

--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -2,11 +2,15 @@ class NotificationsController < ApplicationController
   PER_PAGE = 20
 
   def index
-    @notifications = current_user.notifications
-                                  .by_target(params[:status])
-                                  .order(created_at: :desc)
-                                  .preload(notifiable: [ :user, { item: [ :user, :winner ] } ])
-                                  .page(params[:page])
-                                  .per(PER_PAGE)
+    notification_scope = current_user.notifications
+    if params[:status] == "unread"
+      notification_scope = notification_scope.unread
+    elsif params[:status].present?
+      notification_scope = notification_scope.none
+    end
+    @notifications = notification_scope.order(created_at: :desc)
+                                        .preload(notifiable: [ :user, { item: [ :user, :winner ] } ])
+                                        .page(params[:page])
+                                        .per(PER_PAGE)
   end
 end
--- app/models/notification.rb
+++ app/models/notification.rb
@@ -3,13 +3,6 @@ class Notification < ApplicationRecord
   belongs_to :notifiable, polymorphic: true
 
   scope :unread, -> { where(read: false) }
-  scope :by_target, ->(target) {
-  if target == "unread"
-    where(read: false)
-  else
-    all
-  end
-  }
 
   def message
     case notifiable
