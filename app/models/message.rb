1320c34933ace58184b0c60fc000601e64d69abd
Kushizaki <kushimegu288@gmail.com>
2026-06-24 18:30:10 +0900

    メッセージ受信者を指定するメソッドをMessageモデルに移行

--- app/models/message.rb
+++ app/models/message.rb
@@ -7,6 +7,13 @@ class Message < ApplicationRecord
 
   after_create_commit :create_notifications
 
+  def recipient
+    if user.admin? && [ item.user, item.winner ].exclude?(user)
+      return nil
+    end
+    user == item.user ? item.winner : item.user
+  end
+
   private
 
   def create_notifications
