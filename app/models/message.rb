916bfe5c9d254a507b09d6ec115397a504537936
Kushizaki <kushimegu288@gmail.com>
2026-06-27 15:37:46 +0900

    関連の利用の修正

--- app/models/message.rb
+++ app/models/message.rb
@@ -10,9 +10,9 @@ class Message < ApplicationRecord
   private
 
   def create_notifications
-    recipient_id = message.item.other_user_for(message.user).id
+    recipient_id = item.other_user_for(user).id
     return if recipient_id.nil?
-    message.notifications.create!(user_id: recipient_id)
+    notifications.create!(user_id: recipient_id)
     NotifyMessageCreatedJob.perform_later(id, recipient_id)
   end
 end
