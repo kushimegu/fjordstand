25d91e47b7c1cbf5e4589b33af8a3555c09dc904
Kushizaki <kushimegu288@gmail.com>
2026-06-28 14:10:44 +0900

    改行が揃うよう修正

--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -4,9 +4,10 @@ class NotificationsController < ApplicationController
   def index
     notification_scope = current_user.notifications
     notification_scope = notification_scope.unread if params[:status] == "unread"
-    @notifications = notification_scope.order(created_at: :desc)
-                                        .preload(notifiable: [ :user, { item: [ :user, :winner ] } ])
-                                        .page(params[:page])
-                                        .per(PER_PAGE)
+    @notifications = notification_scope
+                      .order(created_at: :desc)
+                      .preload(notifiable: [ :user, { item: [ :user, :winner ] } ])
+                      .page(params[:page])
+                      .per(PER_PAGE)
   end
 end
